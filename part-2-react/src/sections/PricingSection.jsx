e3eef445e5e08b3fed12ea6a9673a1439c47c4b5
DoCongSon <sonanhson99@gmail.com>
2025-10-20 20:50:57 +0700

    Start Part 2: Create component structure and css base class

---
+++ part-2-react/src/sections/PricingSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PricingSection = () => {
+  return (
+    <section className='py-16 bg-white rounded-lg shadow-md'>
+      <h2 className='heading-2 text-center'>Pricing & Plan</h2>
+    </section>
+  )
+}
+
+export default PricingSection
